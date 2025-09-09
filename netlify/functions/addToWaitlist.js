42855b97186e27a670db921a65d58e69cc675c0a
snakescrack <your.email@example.com>
2025-09-09 16:29:35 +0100

    fix: Force legacy crypto provider in waitlist function

--- netlify/functions/addToWaitlist.js
+++ netlify/functions/addToWaitlist.js
@@ -1,3 +1,6 @@
+import crypto from 'crypto';
+crypto.setEngine('node:crypto', 'legacy');
+
 import { initializeApp, getApps, cert } from 'firebase-admin/app';
 import { getFirestore } from 'firebase-admin/firestore';
 import fs from 'fs';
